a37f7b0f04e15d11210820f198934ea114c61a37
Ayrat Badykov <ayratin555@gmail.com>
2018-03-23 22:04:27 +0300

    initial commit

---
+++ lib/uploadcare_ex.ex
@@ -0,0 +1,18 @@
+defmodule UploadcareEx do
+  @moduledoc """
+  Documentation for UploadcareEx.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UploadcareEx.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/uploadcare_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule UploadcareExTest do
+  use ExUnit.Case
+  doctest UploadcareEx
+
+  test "greets the world" do
+    assert UploadcareEx.hello() == :world
+  end
+end
