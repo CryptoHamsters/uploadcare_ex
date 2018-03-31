267d9f8d47bafefd2c4dd658cf0759b2219e27ac
Ayrat Badykov <ayratin555@gmail.com>
2018-03-31 10:09:19 +0300

    fix dialyzer

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule UploadcareEx.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [ignore_warnings: "dialyzer.ignore-warnings"]
     ]
   end
 
