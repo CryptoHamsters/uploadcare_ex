6bc27ac11922a736cb7f84e45a72a97945ab6aec
Ivan Ivchenko <waltereevan@gmail.com>
2019-12-02 14:39:09 +0600

    Refactor CNX-384

--- lib/uploadcare_ex/api/upload/file.ex
+++ lib/uploadcare_ex/api/upload/file.ex
@@ -55,10 +55,8 @@ defmodule UploadcareEx.API.Upload.File do
         {"UPLOADCARE_PUB_KEY", Config.public_key()},
         {"UPLOADCARE_STORE", Config.store()},
         {"file", IO.iodata_to_binary(data),
-          {"form-data", [
-            {"name", "\"file\""},
-            {"filename", "\"#{filename}\""}]},
-          [{"Content-Type", content_type}]}
+         {"form-data", [{"name", "\"file\""}, {"filename", "\"#{filename}\""}]},
+         [{"Content-Type", content_type}]}
       ]
     }
   end
